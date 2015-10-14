4dad4755abd717ae7edcf18ad843523ceb1eb1f5
PegasusWang <291374108@qq.com>
2015-10-14 23:37:54 +0800

    modify

--- text_html/to_utf8.py
+++ text_html/to_utf8.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
-def dos2unix(filename):
-    text = open(filename, 'rb').read().replace('\r\n', '\n')
-    open(filename, 'wb').write(text)
+import os
 
 
+def get_all_htm(path):
+    res = set()
+    list_dirs = os.walk(path)
+    for root, dirs, files in list_dirs:
+        for f in files:
+            if f.endswith('.htm'):
+                res.add(os.path.join(root, f))
+    return res
+
+
+'''
+请注意 gb2312 不是 “gb2312”，凡 gb2312 的请换成 gb18030.
+微软将 gb2312 和 gbk 映射为 gb18030，方便了一些人，也迷惑了一些人。
+'''
 def to_utf8(filename):
-    txt = open(filename, 'r').read()
-    with open(filename, 'w') as f:
-        if 'charset=gb2312' in txt.lower():
-            txt = txt.decode('gb2312')
-        else:
-            txt = txt.decode('gbk')
-        f.write(txt.encode('utf-8'))
+    lines = open(filename, 'r').readlines()
+    lines = [i.replace('\r\n', '\n').strip() for i in lines]
+    for i, line in enumerate(lines):
+        if 'utf-8' in line:
+            lines[i] = ''
+            continue
+        if 'charset=gb2312' in line:
+            lines[i] = line.replace('charset=gb2312', 'charset=utf-8')
+            continue
+        elif 'charset=gbk' in line:
+            lines[i] = line.replace('charset=gbk', 'charset=utf-8')
 
-#暂时不用转成utf－8，抠出来想要得信息,其他不用管
+    txt = '\n'.join(lines)
+    content = txt.decode('gb18030').encode('utf-8')
+    filename += 'l'
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
 def main():
-    filename = './t.html'
-    fetch(filename)
-    dos2unix(filename)
-    to_utf8(filename)
+    htm_files = get_all_htm('.')
+    for f in htm_files:
+        print(f)
+        to_utf8(f)
 
 
-main()
+if __name__ == '__main__':
+    main()
