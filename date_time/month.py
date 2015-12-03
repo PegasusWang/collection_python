23f6c50504162bc0009a4c5408a8c3e3cd9af932
Pegasus Wang <tianma201211@gmail.com>
2015-12-03 14:41:29 +0800

    add new file handle month

---
+++ date_time/month.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+from datetime import datetime, timedelta
+
+def diff_month(st_year, st_month, end_year, end_month):
+    """计算月分差"""
+    d1 = datetime(year=st_year, month=st_month, day=1)
+    d2 = datetime(year=end_year, month=end_month, day=1)
+    return abs((d1.year - d2.year)*12 + d1.month - d2.month)
+
+
+def month_year_iter( start_month, start_year, end_month, end_year ):
+    ym_start= 12*start_year + start_month - 1
+    ym_end= 12*end_year + end_month - 1
+    for ym in range( ym_start, ym_end ):
+        y, m = divmod( ym, 12 )
+        yield y, m+1
+
+
+def _month_year_iter(start_year, start_month, end_year, end_month):
+    ym_start= 12*start_year + start_month - 1
+    ym_end= 12*end_year + end_month - 1
+    for ym in range( ym_start, ym_end ):
+        y, m = divmod( ym, 12 )
+        yield y, m+1
+
+def main():
+    st_year = 2015
+    st_month = 1
+    data = [1,2,3,4]
+    beg = (diff_month(st_year,st_month,2015,1))
+    end = (diff_month(st_year,st_month,2015,7))
+    print(data[beg:end])
+
+
+def test():
+    #for year, month in month_year_iter(8,2014,7,2015):
+    for year, month in _month_year_iter(2014,8,2015,7):
+        print(year, month)
+
+if __name__ == '__main__':
+    test()
