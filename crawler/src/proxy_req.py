e64150934d9121c70b21376ed99bc417afe976bf
Pegasus Wang <tianma201211@gmail.com>
2015-12-23 14:29:05 +0800

    add proxy_req

---
+++ crawler/src/proxy_req.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import requests
+
+# install lantern first
+proxies = {
+    "http": "http://127.0.0.1:8787",
+    "https": "http://127.0.0.1:8787",
+}
+
+url = 'http://facebook.com'
+r = requests.get(url, proxies=proxies)
+print r.text
+
