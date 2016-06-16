b30330cd94fc469a54452bc17250328694f2ed06
PegasusWang <291374108@qq.com>
2016-06-16 18:02:41 +0800

    modify encoding tool

--- text_html/encoding_decoding_tool.py
+++ text_html/encoding_decoding_tool.py
@@ -2,27 +2,37 @@
 # -*- coding:utf-8 -*-
 
 # pip install cchardet
-import cchardet
+try:
+    import cchardet as chardet
+except ImportError:
+    import chardet
 import requests
 
 
-def get_encoding(data):
-    return cchardet.detect(data)['encoding']
+def get_encoding(unknown_bytes):
+    return chardet.detect(unknown_bytes)['encoding']
 
 
 def convert_encoding(data, new_coding='UTF-8'):
     """未知编码转成utf8"""
-    encoding = cchardet.detect(data)['encoding']
+    encoding = chardet.detect(data)['encoding']
     if new_coding.upper() != encoding.upper():
         data = data.decode(encoding, data).encode(new_coding)
     return data
 
 
 def to_unicode(unknown_bytes):
-    encoding = cchardet.detect(unknown_bytes)['encoding']
+    encoding = chardet.detect(unknown_bytes)['encoding']
     return unknown_bytes.decode(encoding)
 
 
 def detect_html_encoding(url):
-    r = requests.get(url).content
-    return cchardet.detect(data)['encoding']
+    data = requests.get(url).content
+    return chardet.detect(data)['encoding']
+
+
+if __name__ == '__main__':
+    print detect_html_encoding('http://www.baidu.com')
+    convert_encoding('hehe', new_coding='UTF-8')
+    to_unicode('hehe')
+    print get_encoding('hehe')
