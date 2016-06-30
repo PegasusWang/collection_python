b8118194009aadd971381e7d2872c5b40a087e7e
PegasusWang <291374108@qq.com>
2016-06-30 10:29:01 +0800

    modify enum

--- design_pattern/enum_t.py
+++ design_pattern/enum_t.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+# http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+# python <3.4 pip install enum34
+
 from enum import Enum, IntEnum
 
 '''
@@ -16,25 +20,3 @@ Type = enum(
 #    def __new__(cls):
 #        members = [attr for attr in dir(Example()) if not callable(attr) and not attr.startswith("__")]
 '''
-
-
-def to_value(cls):
-    # members = {
-        # attr: getattr(cls, attr).value
-        # for attr in dir(cls) if not callable(attr) and
-        # not attr.startswith("__")
-    # }
-    #print(type(getattr(cls, 'a')))
-    value = getattr(cls, 'a').value
-    setattr(cls, 'a', value)
-    return cls
-
-
-@to_value
-class T(Enum):
-    a = 1
-    b = 2
-
-
-print(T.a)
-#print(type(T.a))
