a8df29c13c877fdbc40a4112cfb1f36f2a399942
PegasusWang <291374108@qq.com>
2016-07-21 07:55:32 +0800

    add test

--- crawler/src/tmp.py
+++ crawler/src/tmp.py
@@ -1,191 +1,153 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-"""
-chrome有个功能，对于请求可以直接右键copy as curl，然后在命令行里边用curl
-模拟发送请求。现在需要把此curl字符串处理成requests库可以传入的参数格式，
-http://stackoverflow.com/questions/23118249/whats-the-difference-between-request-payload-vs-form-data-as-seen-in-chrome
-"""
-
-import json
-import re
+import time
+import logging
+from datetime import timedelta
+from tornado import httpclient, gen, ioloop, queues
 import traceback
-import requests
-
-from functools import wraps
-from pprint import pprint
+from bs4 import BeautifulSoup
 
 
-def encode_to_dict(encoded_str):
-    """ 将encode后的数据拆成dict
-    >>> encode_to_dict('name=foo')
-    {'name': foo'}
-    >>> encode_to_dict('name=foo&val=bar')
-    {'name': 'foo', 'val': 'var'}
-    """
-
-    pair_list = encoded_str.split('&')
-    d = {}
-    for pair in pair_list:
-        if pair:
-            key = pair.split('=')[0]
-            val = pair.split('=')[1]
-            d[key] = val
-    return d
+def logged(class_):
+    logging.basicConfig(level=logging.INFO)
+    class_.logger = logging.getLogger(class_.__name__)
+    return class_
 
 
-def parse_curl_str(s):
-    """convert chrome curl string to url, headers_dict and data"""
-    pat = re.compile("'(.*?)'")
-    str_list = [i.strip() for i in re.split(pat, s)]   # 拆分curl请求字符串
+@logged
+class AsyncSpider(object):
+    """A simple class of asynchronous spider."""
+    def __init__(self, urls=None, concurrency=10, results=None,
+                 sleep=None, **kwargs):
+        super(AsyncSpider, self).__init__(**kwargs)
 
-    url = ''
-    headers_dict = {}
-    data = ''
+        self.concurrency = concurrency
+        self._q = queues.Queue()
+        self._fetching = set()
+        self._fetched = set()
+        self.sleep = sleep
+        self.urls = urls or []
+        if results is None:
+            self.results = []
+        if not self.urls:
+            self.init_urls()
+        for url in urls:
+            self._q.put(url)
+        httpclient.AsyncHTTPClient.configure(
+            "tornado.curl_httpclient.CurlAsyncHTTPClient"
+        )
 
-    for i in range(0, len(str_list)-1, 2):
-        arg = str_list[i]
-        string = str_list[i+1]
+    def init_urls(self):
+        """init_urls generate urls to self.urls"""
+        raise NotImplementedError
 
-        if arg.startswith('curl'):
-            url = string
+    def fetch(self, url, **kwargs):
+        fetch = getattr(httpclient.AsyncHTTPClient(), 'fetch')
+        http_request = httpclient.HTTPRequest(url, **kwargs)
+        return fetch(http_request, raise_error=False)
 
-        elif arg.startswith('-H'):
-            header_key = string.split(':', 1)[0].strip()
-            header_val = string.split(':', 1)[1].strip()
-            headers_dict[header_key] = header_val
+    def handle_html(self, url, html):
+        """处理html页面"""
+        print(url)
 
-        elif arg.startswith('--data'):
-            data = string
+    def handle_response(self, url, response):
+        """处理http响应，对于200响应码直接处理html页面，
+        否则按照需求处理不同响应码"""
+        if response.code == 200:
+            self.handle_html(url, response.body)
 
-    return url, headers_dict, data
+        elif response.code == 599:    # retry
+            self._fetching.remove(url)
+            self._q.put(url)
+
+    @gen.coroutine
+    def get_page(self, url):
+        if self.sleep is not None:
+            yield gen.sleep(self.sleep)    # sleep when need
+        try:
+            response = yield self.fetch(url)
+            self.logger.debug('######fetched %s' % url)
+        except Exception as e:
+            self.logger.debug('Exception: %s %s' % (e, url))
+            raise gen.Return(e)
+        raise gen.Return(response)    # py3 can just return response
+
+    @gen.coroutine
+    def _run(self):
+        @gen.coroutine
+        def fetch_url():
+            current_url = yield self._q.get()
+            try:
+                if current_url in self._fetching:
+                    return
+
+                self.logger.debug('fetching****** %s' % current_url)
+                self._fetching.add(current_url)
+
+                response = yield self.get_page(current_url)
+                self.handle_response(current_url, response)    # handle reponse
+
+                self._fetched.add(current_url)
+
+            finally:
+                self._q.task_done()
+
+        @gen.coroutine
+        def worker():
+            while True:
+                yield fetch_url()
+
+        # Start workers, then wait for the work queue to be empty.
+        for _ in range(self.concurrency):
+            worker()
+
+        yield self._q.join(timeout=timedelta(seconds=300000))
+
+        try:
+            assert self._fetching == self._fetched
+        except AssertionError:    # some http error not handle
+            self.logger.debug(self._fetching-self._fetched)
+            self.logger.debug(self._fetched-self._fetching)
+
+    def run(self):
+        io_loop = ioloop.IOLoop.current()
+        io_loop.run_sync(self._run)
 
 
-def retry(retries=3):
-    """一个失败请求重试，或者使用下边这个功能强大的retrying
-    pip install retrying
-    https://github.com/rholder/retrying
+class MySpider(AsyncSpider):
 
-    :param retries: number int of retry times.
-    """
-    def _retry(func):
-        @wraps(func)
-        def _wrapper(*args, **kwargs):
-            index = 0
-            while index < retries:
-                index += 1
-                try:
-                    response = func(*args, **kwargs)
-                    if response.status_code == 404:
-                        print(404)
-                        break
-                    elif response.status_code != 200:
-                        print(response.status_code)
-                        continue
-                    else:
-                        break
-                except Exception as e:
-                    traceback.print_exc()
-                    response = None
-            return response
-        return _wrapper
-    return _retry
-
-
-_get = requests.get
-
-
-@retry(5)
-def get(*args, **kwds):
-    if 'timeout' not in kwds:
-        kwds['timeout'] = 10
-    if 'headers' not in kwds:
+    def fetch(self, url, **kwargs):
+        """重写父类fetch方法可以添加cookies，headers，timeout等信息"""
+        cookies_str = "PHPSESSID=j1tt66a829idnms56ppb70jri4; pspt=%7B%22id%22%3A%2233153%22%2C%22pswd%22%3A%228835d2c1351d221b4ab016fbf9e8253f%22%2C%22_code%22%3A%22f779dcd011f4e2581c716d1e1b945861%22%7D; key=%E9%87%8D%E5%BA%86%E5%95%84%E6%9C%A8%E9%B8%9F%E7%BD%91%E7%BB%9C%E7%A7%91%E6%8A%80%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8; think_language=zh-cn; SERVERID=a66d7d08fa1c8b2e37dbdc6ffff82d9e|1444973193|1444967835; CNZZDATA1254842228=1433864393-1442810831-%7C1444972138"
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+            'User-Agent': 'mozilla/5.0 (compatible; baiduspider/2.0; +http://www.baidu.com/search/spider.html)',
+            'cookie': cookies_str
         }
-        kwds['headers'] = headers
+        return super(MySpider, self).fetch(
+            url, headers=headers,
+            proxy_host="127.0.0.1", proxy_port=8787,    # for proxy
+        )
 
-    return _get(*args, **kwds)
-
-requests.get = get
-
-
-def retry_get_html(*args, **kwargs):
-    try:
-        return get(*args, **kwargs).content
-    except AttributeError:
-        return ''
+    def handle_html(self, url, html):
+        print(url)
+        print(html)
+        #print(BeautifulSoup(html, 'lxml').find('title'))
 
 
-def lazy_property(fn):
-    attr_name = '_lazy_' + fn.__name__
-
-    @property
-    def _lazy_property(self):
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, fn(self))
-            return getattr(self, attr_name)
-    return _lazy_property
-
-
-def my_ip():
-    # url = 'https://api.ipify.org?format=json'
-    url = 'http://httpbin.org/ip'
-    return requests.get(url).text
+def main():
+    st = time.time()
+    urls = []
+    #n = 1000
+    n = 2
+    for page in range(1, n):
+        urls.append("""http://www.lagou.com/jobs/positionAjax.json?city=%E5%8C%97%E4%BA%AC&needAddtionalResult=false&qq={}""".format(page))
+    s = MySpider(urls, 10)
+    s.run()
+    print(time.time()-st)
+    print(60.0/(time.time()-st)*1000, 'per minute')
+    print(60.0/(time.time()-st)*1000/60.0, 'per second')
 
 
-def form_data_to_dict(s):
-    """form_data_to_dict s是从chrome里边复制得到的form-data表单里的字符串，
-    注意*必须*用原始字符串r""
-
-    :param s: form-data string
-    """
-    arg_list = [line.strip() for line in s.split('\n')]
-    d = {}
-    for i in arg_list:
-        if i:
-            k = i.split(':', 1)[0].strip()
-            v = ''.join(i.split(':', 1)[1:]).strip()
-            d[k] = v
-    return d
-
-
-def test():
-    from urllib import urlencode
-    #S = """curl 'https://graph.facebook.com/v2.6/act_858751837556226/customaudiences?access_token=EAAI4BG12pyIBAF58zmAHxtmL3iBMY8RCiGgdeMiLBedZC6X6YqZBDdHc0zcEmPT9dPtq1apZC6TgK5m9iENrJKakdW7myLNki6uYhp78PpmZATL6fxMt38QqnUoTV7nrysZBleelH4zV00ITame85PuwoHqQTQ87NVC8scx5kEAZDZD' -H 'origin: https://business.facebook.com' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4' -H 'user-agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'referer: https://business.facebook.com/' -H 'authority: graph.facebook.com' --data '__business_id=712764242174174&_reqName=path%3A%2Fact_858751837556226%2Fcustomaudiences&_reqSrc=adsDaoGraphDataMutator&accountId=858751837556226&creation_params=%7B%22combination_type%22%3A%22website%22%2C%22traffic_type%22%3A3%7D&description=&endpoint=%2Fact_858751837556226%2Fcustomaudiences&exclusions=%5B%7B%22id%22%3Anull%2C%22type%22%3A%22website%22%2C%22retention_days%22%3A30%2C%22rule%22%3A%22%7B%5C%22and%5C%22%3A%5B%7B%5C%22or%5C%22%3A%5B%7B%5C%22url%5C%22%3A%7B%5C%22i_contains%5C%22%3A%5C%22shoe%5C%22%7D%7D%5D%7D%5D%7D%22%2C%22rule_aggregation%22%3Anull%7D%5D&inclusions=%5B%7B%22id%22%3Anull%2C%22type%22%3A%22website%22%2C%22retention_days%22%3A30%2C%22rule%22%3A%22%7B%5C%22and%5C%22%3A%5B%7B%5C%22or%5C%22%3A%5B%7B%5C%22url%5C%22%3A%7B%5C%22i_contains%5C%22%3A%5C%22show%5C%22%7D%7D%5D%7D%5D%7D%22%2C%22rule_aggregation%22%3Anull%7D%5D&locale=zh_CN&method=post&name=759331&pixel_id=1792170334346121&prefill=true&pretty=0&subtype=combination&suppress_http_code=1' --compressed"""
-    S = """curl 'https://graph.facebook.com/v2.5/act_858751837556226/customaudiences?access_token=EAAI4BG12pyIBAF58zmAHxtmL3iBMY8RCiGgdeMiLBedZC6X6YqZBDdHc0zcEmPT9dPtq1apZC6TgK5m9iENrJKakdW7myLNki6uYhp78PpmZATL6fxMt38QqnUoTV7nrysZBleelH4zV00ITame85PuwoHqQTQ87NVC8scx5kEAZDZD' -H 'origin: https://business.facebook.com' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4' -H 'user-agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'referer: https://business.facebook.com/' -H 'authority: graph.facebook.com' --data '__business_id=712764242174174&_reqName=path%3A%2Fact_858751837556226%2Fcustomaudiences&_reqSrc=adsDaoGraphDataMutator&accountId=858751837556226&creation_params=%7B%22combination_type%22%3A%22website%22%2C%22traffic_type%22%3A3%7D&description=&endpoint=%2Fact_858751837556226%2Fcustomaudiences&exclusions=%5B%7B%22id%22%3Anull%2C%22type%22%3A%22website%22%2C%22retention_days%22%3A30%2C%22rule%22%3A%22%7B%5C%22and%5C%22%3A%5B%7B%5C%22or%5C%22%3A%5B%7B%5C%22url%5C%22%3A%7B%5C%22i_contains%5C%22%3A%5C%22shoe%5C%22%7D%7D%5D%7D%5D%7D%22%2C%22rule_aggregation%22%3Anull%7D%5D&inclusions=%5B%7B%22id%22%3Anull%2C%22type%22%3A%22website%22%2C%22retention_days%22%3A30%2C%22rule%22%3A%22%7B%5C%22and%5C%22%3A%5B%7B%5C%22or%5C%22%3A%5B%7B%5C%22url%5C%22%3A%7B%5C%22i_contains%5C%22%3A%5C%22show%5C%22%7D%7D%5D%7D%5D%7D%22%2C%22rule_aggregation%22%3Anull%7D%5D&locale=zh_CN&method=post&name=759331&pixel_id=1792170334346121&prefill=true&pretty=0&subtype=combination&suppress_http_code=1' --compressed"""
-
-    url, headers, data = parse_curl_str(S)
-    pprint(url)
-    pprint(headers)
-    pprint(data)
-
-    params = r"""
-    __business_id:712764242174174
-    _reqName:path:/act_858751837556226/customaudiences
-    _reqSrc:adsDaoGraphDataMutator
-    accountId:858751837556226
-    creation_params:{"combination_type":"website","traffic_type":3}
-    description:
-    endpoint:/act_858751837556226/customaudiences
-    exclusions:[{"id":null,"type":"website","retention_days":30,"rule":"{\"and\":[{\"or\":[{\"url\":{\"i_contains\":\"shoe\"}}]}]}","rule_aggregation":null}]
-    inclusions:[{"id":null,"type":"website","retention_days":30,"rule":"{\"and\":[{\"or\":[{\"url\":{\"i_contains\":\"show\"}}]}]}","rule_aggregation":null}]
-    locale:zh_CN
-    method:post
-    name:759331
-    pixel_id:1792170334346121
-    prefill:true
-    pretty:0
-    subtype:combination
-    suppress_http_code:1
-    """
-    form_data_dict = form_data_to_dict(params)
-    form_str = urlencode(form_data_dict)
-    r = requests.post(url, headers=headers, data=form_str)
-    #r = requests.post(url, headers=headers, data=form_str)
-    url = 'http://httpbin.org/post'
-    r = requests.post(url, headers=headers, data=form_data_dict)
-    print(r.content)
-
-
-test()
+if __name__ == '__main__':
+    main()
