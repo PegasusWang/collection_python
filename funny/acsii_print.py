aec3f6022c44b9058924e8067fa88942f3d778d9
PeasusWang <291374108@qq.com>
2019-09-16 08:47:36 +0800

    add img compress demo

---
+++ funny/acsii_print.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""生成 ascii 字体
+pip install colorama termcolor pyfiglet
+
+"""
+
+import sys
+
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint
+from pyfiglet import figlet_format
+
+# cprint(figlet_format('missile!', font='starwars'), 'yellow', 'on_red', attrs=['bold'])
+cprint(figlet_format(u'docker vim'))
+
+"""
+
+    ____        _   _                  __        __   _          ____       _     _
+|  _ \ _   _| |_| |__   ___  _ __   \ \      / /__| |__      / ___|_   _(_) __| | ___
+| |_) | | | | __| '_ \ / _ \| '_ \   \ \ /\ / / _ \ '_ \    | |  _| | | | |/ _` |/ _ \
+|  __/| |_| | |_| | | | (_) | | | |   \ V  V /  __/ |_) |   | |_| | |_| | | (_| |  __/
+|_|    \__, |\__|_| |_|\___/|_| |_|    \_/\_/ \___|_.__/     \____|\__,_|_|\__,_|\___|
+        |___/
+
+"""
---
+++ img/compress_image.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import os
+from PIL import Image
+
+
+def get_size(file):
+    # 获取文件大小:KB
+    size = os.path.getsize(file)
+    return size / 1024
+
+
+def get_outfile(infile, outfile):
+    if outfile:
+        return outfile
+    dir, suffix = os.path.splitext(infile)
+    outfile = '{}-out{}'.format(dir, suffix)
+    return outfile
+
+
+def compress_image(infile, outfile='', mb=150, step=10, quality=80):
+    """不改变图片尺寸压缩到指定大小
+    https://www.cnblogs.com/li1992/p/10675769.html
+    :param infile: 压缩源文件
+    :param outfile: 压缩文件保存地址
+    :param mb: 压缩目标，KB
+    :param step: 每次调整的压缩比率
+    :param quality: 初始压缩比率
+    :return: 压缩文件地址，压缩文件大小
+    """
+    o_size = get_size(infile)
+    if o_size <= mb:
+        return infile
+    outfile = get_outfile(infile, outfile)
+    while o_size > mb:
+        im = Image.open(infile)
+        im.save(outfile, quality=quality)
+        if quality - step < 0:
+            break
+        quality -= step
+        o_size = get_size(outfile)
+    return outfile, get_size(outfile)
