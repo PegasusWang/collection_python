6dc6a3c43de9be427377ede85d613f50fb3f5a83
PeasusWang <291374108@qq.com>
2019-11-15 21:31:24 +0800

    add sendkey.py

--- mac/sendkey.py
+++ mac/sendkey.py
@@ -2,6 +2,10 @@
 
 """
 使用 python 控制 mac 键盘
+https://pypi.org/project/keyboard/
+https://pythonhosted.org/pynput/keyboard.html
+
+pip install pynput
 """
 
 import time
@@ -11,11 +15,39 @@ keyboard = Controller()
 
 time.sleep(3)
 # Press and release space
-keyboard.press(Key.space)
-keyboard.release(Key.space)
+keyboard.press('f')
+keyboard.release('f')
+time.sleep(0.5)
+keyboard.press('s')
+keyboard.release('s')
+keyboard.press('w')
+keyboard.release('w')
+
+time.sleep(0.5)
+keyboard.press('f')
+keyboard.release('f')
+time.sleep(0.5)
+keyboard.press('d')
+keyboard.release('d')
+keyboard.press('w')
+keyboard.release('w')
+
+time.sleep(3) #删除时间久
+keyboard.press('f')
+keyboard.release('f')
+time.sleep(1)
+keyboard.press('s')
+keyboard.release('s')
+time.sleep(0.5)
+keyboard.press('w')
+keyboard.release('w')
+
 #
 # # Type a lower case A; this will work even if no key on the
 # # physical keyboard is labelled 'A'
+# Press and release space
+# keyboard.press(Key.space)
+# keyboard.release(Key.space)
 # keyboard.press('a')
 # keyboard.release('a')
 #
