75a66318656a2df90ad95b05c38c73d9f4126373
PegasusWang <291374108@qq.com>
2021-01-24 10:03:26 +0800

    feat: 2020-04 danjuan.py

--- fund/danjuan.py
+++ fund/danjuan.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-抓取蛋卷基金数据。主要是为了导出我关注的一些优秀主动基金的季度重仓股。随手写的仅供参考
+抓取蛋卷基金数据。主要是为了导出我关注的一些优秀主动基金的季度重仓股
 
-pip install requests
-pip install SQLAlchemy -i https://pypi.doubanio.com/simple --user
-pip install pymysql -i https://pypi.doubanio.com/simple --user
+pip3 install requests
+pip3 install SQLAlchemy -i https://pypi.doubanio.com/simple --user
+pip3 install pymysql -i https://pypi.doubanio.com/simple --user
 
-# https://github.com/numpy/numpy/issues/15947 numpy 版本高 mac 有问题
+# https://github.com/numpy/numpy/issues/15947 numpy 版本高有问题
 pip3 install numpy==1.18.0 -i https://pypi.doubanio.com/simple
 pip3 install pandas -i https://pypi.doubanio.com/simple
 pip3 install openpyxl -i https://pypi.doubanio.com/simple
@@ -24,18 +24,43 @@ import requests
 import sqlalchemy as db
 
 """
-curl 'https://danjuanfunds.com/djapi/fund/detail/110011' \
+curl 'https://danjuanfunds.com/djapi/fund/detail/005827' \
   -H 'Connection: keep-alive' \
   -H 'Accept: application/json, text/plain, */*' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36' \
-  -H 'elastic-apm-traceparent: 00-ce58f82d3f8e8e6fcd54397fd0f38574-adb2e157029a1192-01' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36' \
+  -H 'elastic-apm-traceparent: 00-8936c0d09ef99045716fac06ff3777a0-3df470af2db9906a-01' \
   -H 'Sec-Fetch-Site: same-origin' \
   -H 'Sec-Fetch-Mode: cors' \
   -H 'Sec-Fetch-Dest: empty' \
-  -H 'Referer: https://danjuanfunds.com/funding/110011?channel=1300100141' \
+  -H 'Referer: https://danjuanfunds.com/funding/005827?channel=1300100141' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7' \
-  -H 'Cookie: device_id=web_SkS2df508; _ga=GA1.2.1397406205.1593612461; gr_user_id=2ac24d8b-927e-475d-8f29-27589058f70f; Hm_lvt_d8a99640d3ba3fdec41370651ce9b2ac=1602344637,1602344758,1602344764,1603369369; acw_tc=2760822016038852981828649e5a0c60b368285cb530c1a2e8d169c1867d83; xq_a_token=c2974070ad952835feab798d5278f70696c9f25c; Hm_lpvt_d8a99640d3ba3fdec41370651ce9b2ac=1603885299; channel=1300100141; timestamp=1603885317702' \
+  -H 'Cookie: device_id=web_SkS2df508; _ga=GA1.2.1397406205.1593612461; gr_user_id=2ac24d8b-927e-475d-8f29-27589058f70f; channel=1300100141; xq_a_token=d79103ea23560cd344415ed151659059a3397776; Hm_lvt_d8a99640d3ba3fdec41370651ce9b2ac=1609645857; acw_tc=2760776e16113729412211916e762552b2c598aae61864775cda6af8bb016f; Hm_lpvt_d8a99640d3ba3fdec41370651ce9b2ac=1611373570; timestamp=1611373569899' \
   --compressed
+
+requests.get("https://danjuanfunds.com/djapi/fund/detail/005827",
+    headers={
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
+        "Referer": "https://danjuanfunds.com/funding/005827?channel=1300100141",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+        "elastic-apm-traceparent": "00-8936c0d09ef99045716fac06ff3777a0-3df470af2db9906a-01"
+    },
+    cookies={
+        "Hm_lpvt_d8a99640d3ba3fdec41370651ce9b2ac": "1611373570",
+        "Hm_lvt_d8a99640d3ba3fdec41370651ce9b2ac": "1609645857",
+        "_ga": "GA1.2.1397406205.1593612461",
+        "acw_tc": "2760776e16113729412211916e762552b2c598aae61864775cda6af8bb016f",
+        "channel": "1300100141",
+        "device_id": "web_SkS2df508",
+        "gr_user_id": "2ac24d8b-927e-475d-8f29-27589058f70f",
+        "timestamp": "1611373569899",
+        "xq_a_token": "d79103ea23560cd344415ed151659059a3397776"
+    },
+)
 """
 
 
@@ -49,22 +74,24 @@ def get_fund_json(fund_code, fund_name="", to_file=False):
             "Accept": "application/json, text/plain, */*",
             "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
             "Connection": "keep-alive",
-            "Referer": "https://danjuanfunds.com/funding/110011?channel=1300100141",
+            "Referer": "https://danjuanfunds.com/funding/005827?channel=1300100141",
             "Sec-Fetch-Dest": "empty",
             "Sec-Fetch-Mode": "cors",
             "Sec-Fetch-Site": "same-origin",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
-            "elastic-apm-traceparent": "00-ce58f82d3f8e8e6fcd54397fd0f38574-adb2e157029a1192-01"},
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+            "elastic-apm-traceparent": "00-8936c0d09ef99045716fac06ff3777a0-3df470af2db9906a-01",
+        },
         cookies={
-            "Hm_lpvt_d8a99640d3ba3fdec41370651ce9b2ac": "1603885299",
-            "Hm_lvt_d8a99640d3ba3fdec41370651ce9b2ac": "1602344637,1602344758,1602344764,1603369369",
+            "Hm_lpvt_d8a99640d3ba3fdec41370651ce9b2ac": "1611373570",
+            "Hm_lvt_d8a99640d3ba3fdec41370651ce9b2ac": "1609645857",
             "_ga": "GA1.2.1397406205.1593612461",
-            "acw_tc": "2760822016038852981828649e5a0c60b368285cb530c1a2e8d169c1867d83",
+            "acw_tc": "2760776e16113729412211916e762552b2c598aae61864775cda6af8bb016f",
             "channel": "1300100141",
             "device_id": "web_SkS2df508",
             "gr_user_id": "2ac24d8b-927e-475d-8f29-27589058f70f",
-            "timestamp": "1603885317702",
-            "xq_a_token": "c2974070ad952835feab798d5278f70696c9f25c"},
+            "timestamp": "1611373569899",
+            "xq_a_token": "d79103ea23560cd344415ed151659059a3397776",
+        },
     )
     jsontext = resp.text  # 重定向到 json 文件可以格式化查看
 
@@ -78,6 +105,7 @@ def get_fund_json(fund_code, fund_name="", to_file=False):
 
 Connection = None
 Table = None
+TableName = "danjuan_fund_2020_4"  # 2020 四季度基金
 
 
 def init_conn():
@@ -86,14 +114,14 @@ def init_conn():
     engine = db.create_engine(url)
     metadata = db.MetaData()
     Connection = engine.connect()
-    Table = db.Table('danjuan_fund', metadata, autoload=True, autoload_with=engine)
+    Table = db.Table(TableName, metadata, autoload=True, autoload_with=engine)
 
 
 def fund_name_from_manager_list(fund_code, achievement_list):
     """每个管理者都会管理很多基金，找到当前这个基金并返回名字"""
     for fund in achievement_list:
-        if fund.get('fund_code') == fund_code:
-            return fund['fundsname']
+        if fund.get("fund_code") == fund_code:
+            return fund["fundsname"]
     return ""
 
 
@@ -101,20 +129,20 @@ def get_managers(manager_list):
     """找到所有基金管理人返回空格分割字符串 eg: 李晓星 张坤"""
     name_list = []
     for manager in manager_list:
-        name_list.append(manager['name'])
+        name_list.append(manager["name"])
     return " ".join(name_list)
 
 
 def parse_danjuan_fund(fund_code, json_text):
     d = json.loads(json_text)
-    data = d['data']
-    manager_list = data['manager_list']
-    achievement_list = manager_list[0]['achievement_list']  # 找到一个管理者
+    data = d["data"]
+    manager_list = data["manager_list"]
+    achievement_list = manager_list[0]["achievement_list"]  # 找到一个管理者
 
     fund_name = fund_name_from_manager_list(fund_code, achievement_list)
     managers = get_managers(manager_list)
     try:
-        enddate = data['fund_position']['enddate']  # 季报披露日期
+        enddate = data["fund_position"]["enddate"]  # 季报披露日期
     except KeyError:
         enddate = ""
     return fund_name, managers, enddate
@@ -122,7 +150,7 @@ def parse_danjuan_fund(fund_code, json_text):
 
 # 在你的 mysql 创建这个表
 """
-CREATE TABLE `danjuan_fund` (
+CREATE TABLE `danjuan_fund_2020_4` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `fund_name` varchar(64) DEFAULT '' COMMENT '基金名称',
     `fund_code` varchar(16) NOT NULL DEFAULT '' COMMENT '基金代码',
@@ -154,46 +182,33 @@ def request_and_save(fund_code, fund_name):
 
 
 def get_my_xueqiu_fund_codes():
-    """ 从我的雪球获取我关注的所有基金代码。如果你有雪球账号并且有关注的基金，可以用这段代码自动化查询。
+    """从我的雪球获取我关注的所有基金代码
     https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?size=1000&pid=-110&category=2
     """
-    # resp = requests.get(
-    #     "https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?size=1000&pid=-110&category=2",
-    #     headers={
-    #         "Accept": "application/json, text/plain, */*",
-    #         "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    #         "Connection": "keep-alive",
-    #         "Origin": "https://xueqiu.com",
-    #         "Referer": "https://xueqiu.com/",
-    #         "Sec-Fetch-Dest": "empty",
-    #         "Sec-Fetch-Mode": "cors",
-    #         "Sec-Fetch-Site": "same-site",
-    #         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"},
-    #     cookies={}, # TODO 用浏览器查询请求，改成你自己的 uncurl requests 代码，注意你的 cookie 不要随便泄露出去
-    # )
-    #
-    # with open("./funds.json", "w") as f:
-    #     json.dump(resp.json(), f, indent=2, ensure_ascii=False)
-    #
+
+    # 直接从雪球上导出到本地 json 文件吧，cookie 之前的会过期
+    with open("./funds.json", "w") as f:
+        json.dump(resp.json(), f, indent=2, ensure_ascii=False)
+
     codes = []  # [ (code, name) ]
     with open("./funds.json") as f:
         res = json.load(f)
-        stocks = res['data']['stocks']
+        stocks = res["data"]["stocks"]
         for stock in stocks:
-            symbol = stock['symbol']
-            fund_code = ''.join(char for char in symbol if char.isdigit())
-            fund_name = stock['name']
+            symbol = stock["symbol"]
+            fund_code = "".join(char for char in symbol if char.isdigit())
+            fund_name = stock["name"]
             codes.append((fund_code, fund_name))
     return codes
 
 
 def crawl_all_my_funds_to_mysql():
     funds = get_my_xueqiu_fund_codes()
-    __import__('pprint').pprint(funds)
+    __import__("pprint").pprint(funds)
 
     for fund_code, fund_name in funds:
         request_and_save(fund_code, fund_name)
-        time.sleep(random.randint(5, 10))  # 注意慢一点，随机 sleep 防止命中反作弊
+        time.sleep(random.randint(5, 10))  # 慢一点，防止反作弊命中
 
 
 def export_all_mysql_funds_stocks_to_dict():
@@ -204,17 +219,17 @@ def export_all_mysql_funds_stocks_to_dict():
     for row in rows:
         d = json.loads(row.detail_json)
         try:
-            stock_list = d['data']['fund_position']['stock_list']
+            stock_list = d["data"]["fund_position"]["stock_list"]
         except KeyError:  # 新基金没披露可能为空
             stock_list = []
 
         stocks = []
         for stock in stock_list:
-            name, code, percent = stock['name'], stock['code'], stock['percent']
+            name, code, percent = stock["name"], stock["code"], stock["percent"]
             stock_fmt = u"{}[{}]({}%)".format(name, code, percent)
             stocks.append(stock_fmt)
 
-        if len(stocks) < 10: # 十大重仓股
+        if len(stocks) < 10:
             stocks += (10 - len(stocks)) * [""]
 
         key = row.fund_name
@@ -227,7 +242,7 @@ def export_all_mysql_funds_stocks_to_dict():
 # https://www.geeksforgeeks.org/how-to-create-dataframe-from-dictionary-in-python-pandas/
 def export_all_mysql_funds_stocks_to_excel_vertical():
     fund_dict = export_all_mysql_funds_stocks_to_dict()
-    index = ['代码', '管理人', '季报日期'] + ['重仓股'] * 10  # 十大重仓
+    index = ["代码", "管理人", "季报日期"] + ["重仓股"] * 10  # 十大重仓
     df = pd.DataFrame(fund_dict, index=index)
     df.to_excel("./funds_stock_vertical.xlsx")
 
@@ -235,7 +250,7 @@ def export_all_mysql_funds_stocks_to_excel_vertical():
 def export_all_mysql_funds_stocks_to_excel():  # 横着
     fund_dict = export_all_mysql_funds_stocks_to_dict()
     # index = ['代码', '管理人', '季报日期'] + ['重仓股'] * 10
-    df = pd.DataFrame.from_dict(fund_dict, orient='index')
+    df = pd.DataFrame.from_dict(fund_dict, orient="index")
     df.to_excel("./fund_stock.xlsx")
 
 
@@ -247,24 +262,26 @@ def export_all_stock_funds():
     for row in rows:
         d = json.loads(row.detail_json)
         try:
-            stock_list = d['data']['fund_position']['stock_list']
+            stock_list = d["data"]["fund_position"]["stock_list"]
         except KeyError:  # 新基金没披露可能为空
             stock_list = []
 
         for stock in stock_list:
-            name, code, percent = stock['name'], stock['code'], stock['percent']
-            stock_name = u"{}({})".format(name, code)
+            name, code, percent = stock["name"], stock["code"], stock["percent"]
+            if len(code) == 5:  # 港股目前是 5 位数
+                stock_name = u"{}({})[HK]".format(name, code)
+            else:
+                stock_name = u"{}({})".format(name, code)  # 股票名
             stock_funds[stock_name].append(row.fund_name)
 
     keys = sorted(stock_funds, key=lambda k: len(stock_funds[k]), reverse=True)
     sorted_stock_dict = {k: stock_funds[k] for k in keys}
-    df = pd.DataFrame.from_dict(sorted_stock_dict, orient='index')
+    df = pd.DataFrame.from_dict(sorted_stock_dict, orient="index")
     df.to_excel("./stock.xlsx")
 
 
 def main():
-    # get_fund_json("007300", "汇添富中盘", True) # 单独抓取一个基金数据到文件
-    crawl_all_my_funds_to_mysql() # 抓取所有我关注的雪球上的基金到 mysql
+    crawl_all_my_funds_to_mysql()  # 抓取所有我关注的雪球上的基金到 mysql
 
     export_all_mysql_funds_stocks_to_excel_vertical()  # 导出基金十大重仓股
     export_all_mysql_funds_stocks_to_excel()  # 导出横版基金十大重仓
