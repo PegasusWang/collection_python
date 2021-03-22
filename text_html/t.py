8e323c4a98326ce234163b5dc0d6ec130370d0ad
liming <jj7jump@gmail.com>
2021-03-23 01:01:52 +0800

    bug fix

--- text_html/t.py
+++ text_html/t.py
@@ -18,7 +18,7 @@ def convert_encoding(data, new_coding='UTF-8'):
     """未知编码转成utf8"""
     encoding = cchardet.detect(data)['encoding']
     if new_coding.upper() != encoding.upper():
-        data = data.decode(encoding, data).encode(new_coding)
+        data = data.decode(encoding).encode(new_coding)
     return data
 
 
