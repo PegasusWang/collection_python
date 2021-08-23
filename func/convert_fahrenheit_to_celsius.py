82da954084445d69d71aae49dac15018027c5367
liming <jj7jump@gmail.com>
2021-08-23 21:06:22 +0800

    华氏温度转换为摄氏温度

--- func/convert_fahrenheit_to_celsius.py
+++ func/convert_fahrenheit_to_celsius.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import sys
 
 """
 提示：华氏温度到摄氏温度的转换公式为：$C=(F - 32) \div 1.8$。
@@ -16,10 +17,9 @@ def convert_temperature(temperature):
         print(err)
     else:
         celsius = (fahrenheit - 32) / 1.8
+        print('%.1f华氏度 = %.1f摄氏度' % (fahrenheit, celsius))
         return celsius
 
 
 if __name__ == '__main__':
-    f = 100
-    c = convert_temperature(f)
-    print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+    convert_temperature(100)
