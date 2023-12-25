ba426f0b7f42a0ff21036b7c6bdc264344685e7e
PegasusWang <291374108@qq.com>
2023-12-25 19:25:19 +0800

    fix: 修复go移除旧版本包不同格式版本号对比

--- tools/clean_go_mod_old_version.py
+++ tools/clean_go_mod_old_version.py
@@ -36,7 +36,7 @@ def remove(path, real_remove=False):  # 删除文件夹或者文件
 
 def get_all_sub_dir():
     dirs = [f for f in os.listdir("./") if os.path.isdir(f)]
-    dirs.sort(key=functools.cmp_to_key(cpm_version))  # 排序后每个包只保留最后一个版本
+    dirs.sort(key=functools.cmp_to_key(cmp_version))  # 排序后每个包只保留最后一个版本
     packages = set()
     for d in dirs:
         packagename = d.split("@")[0]
@@ -53,7 +53,7 @@ def get_all_sub_dir():
 
 def remove_packagename_old_version(to_remove_package_name):
     dirs = [f for f in os.listdir("./") if os.path.isdir(f)]
-    dirs.sort(key=functools.cmp_to_key(cpm_version))  # 排序后每个包只保留最后一个版本
+    dirs.sort(key=functools.cmp_to_key(cmp_version))  # 排序后每个包只保留最后一个版本
     packages = set()
     for d in dirs:
         packagename = d.split("@")[0]
@@ -69,28 +69,40 @@ def remove_packagename_old_version(to_remove_package_name):
         remove(remove_dir, True)
 
 
-def cpm_version(dir1, dir2):
+def cmp_version(dir1, dir2):
+    """
+    比较版本号，需要处理多种格式的版本。先获取版本号，然后拆分成两部分 a.b.c 和 后半部分。先比较前半部分，然后比较后半部分
+    base@v0.0.5-rc1
+    commonbiz@v1.1.26
+    novel_fm_mealsdk@v0.0.0-20230322064632-ab19d8ea0f0e
+    """
     s1 = dir1.split("@")[1]
     s2 = dir2.split("@")[1]
-    try:  # 处理合法版本 v1.0.0
-        v1 = version(s1)
-        v2 = version(s2)
-        if v1 < v2:
-            return -1
-        elif v1 == v2:
-            return 0
-        else:
-            return 1
-    except Exception:  # 处理类似这种版本 "v0.0.0-20231011075303-d9230068a794" < "v0.0.0-20231012052358-9836c2e23432"
-        if s1 < s2:
-            return -1
-        elif s1 == s2:
-            return 0
-        else:
-            return 1
+
+    sv1 = s1.split("-")[0]
+    sv2 = s2.split("-")[0]
+
+    # 先比较第一部分
+    v1 = version(sv1)
+    v2 = version(sv2)
+    if v1 < v2:
+        return -1
+    if v1 > v2:
+        return 1
+    # 如果第一部分相等，比较第二部分
+    ss1 = s1.split("-")
+    ss2 = s2.split("-")
+    s1second = ss1[1] if len(ss1) > 1 else ""
+    s2second = ss2[1] if len(ss2) > 1 else ""
+    if s1second < s2second:
+        return -1
+    elif s1second == s2second:
+        return 0
+    else:
+        return 1
 
 
-def cpm_version_test():
+def version_test():
     """
     pip install pytest
     """
@@ -98,6 +110,17 @@ def cpm_version_test():
     assert version("v1.0.0") < version("v1.0.1-rc2")
     assert version("v1.0.87") < version("v1.0.443")
     assert "v0.0.0-20231011075303-d9230068a794" < "v0.0.0-20231012052358-9836c2e23432"
+
+
+def cpm_version_test():
+    assert cmp_version("pkg@v1.0.0", "pkg@v1.0.1") == -1
+    assert cmp_version("pkg@v1.0.0", "pkg@v1.0.1-rc2") == -1
+    assert cmp_version("pkg@v1.0.87", "pkg@v1.0.443") == -1
+    assert cmp_version("pkg@v0.0.0-20231011075303-d9230068a794",
+                       "pkg@v0.0.0-20231012052358-9836c2e23432") == -1
+    assert cmp_version(
+        "pkg@v1.0.24", "pkg@v1.0.29-0.20230703093406-7d8703fcbc29") == -1
+    assert cmp_version("pkg@v1.0.24", "pkg@v1.0.24") == 0
 
 
 def main():
@@ -110,4 +133,5 @@ def main():
 
 
 if __name__ == "__main__":
+    # cpm_version_test()
     main()
